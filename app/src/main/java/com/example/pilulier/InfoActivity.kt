8276f72abe10d18b6ffcd7fe3930b57b62cc3ace
justine <curiejustine@gmail.com>
2025-04-24 08:08:35 +0200

    info fonctionne

--- app/src/main/java/com/example/pilulier/InfoActivity.kt
+++ app/src/main/java/com/example/pilulier/InfoActivity.kt
@@ -3,6 +3,7 @@ package com.example.pilulier
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -17,7 +18,8 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-        val btnBack = findViewById<View>(R.id.btnBackInfo)
+
+        val btnBack = findViewById<ImageButton>(R.id.btnBackInfo)
         btnFuyant = findViewById(R.id.btnFuyant)
         rootLayout = findViewById(R.id.info_main)
 
@@ -33,7 +35,6 @@ class InfoActivity : AppCompatActivity() {
                 val randomX = Random.nextInt(0, maxX)
                 val randomY = Random.nextInt(0, maxY)
 
-                // On applique une nouvelle position avec ConstraintSet
                 val constraintSet = ConstraintSet()
                 constraintSet.clone(rootLayout)
 
